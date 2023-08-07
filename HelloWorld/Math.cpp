22d3b0aab846afd1201fbcee8f9d5f1d7f068fd2
jhonsion <18166038497@163.com>
2023-08-07 23:37:42 +0800

    编译过程的学习

---
+++ HelloWorld/Math.cpp
@@ -0,0 +1,16 @@
+//#define INTEGER int
+//
+//#if 1
+const char* Log(const char* message)
+{
+	return message;
+}
+
+int Multiply(int a, int b)
+{
+	//int result = a * b;
+	Log("Multiply");
+	return a * b;
+}
+//#include "EndBrace.h"
+//#endif
